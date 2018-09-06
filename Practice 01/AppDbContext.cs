de4a32b9e8bb3a6b4ceba36be915af9353042d7e
DirtyOleg <11252872+DirtyOleg@users.noreply.github.com>
2018-09-05 21:36:02 -0400

    Add project files.

---
+++ Practice 01/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Practice_01
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
